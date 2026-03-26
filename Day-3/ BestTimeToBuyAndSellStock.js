91bfbbf9cd532f02856f3b0dc9677f9f1d50a35b
reddirani8496 <reddiranibattuluri@gmail.com>
2026-03-26 22:06:01 +0530

    BestTime to buy and sell stock program uploaded

---
+++ Day-3/ BestTimeToBuyAndSellStock.js
@@ -0,0 +1,19 @@
+// Refer code chef
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let maxProfit = 0;
+  let min = prices[0];
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - min > maxProfit) {
+      maxProfit = prices[i] - min;
+    }
+    if (prices[i] < min) min = prices[i];
+  }
+
+  return maxProfit;
+};
