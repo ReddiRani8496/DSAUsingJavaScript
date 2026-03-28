be6d1e867aefe95c4171c3034dad9b5ff9c97319
reddirani8496 <reddiranibattuluri@gmail.com>
2026-03-28 22:51:44 +0530

    remove element approach2 program uploaded

--- Day-3/Removeelement.js
+++ Day-3/Removeelement.js
@@ -5,14 +5,27 @@
  * @param {number} val
  * @return {number}
  */
+// var removeElement = function (nums, val) {
+//   let x = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] != val) {
+//       nums[x] = nums[i];
+//       x++;
+//     }
+//   }
+
+//   return x;
+// };
+
+//Appr2
+
 var removeElement = function (nums, val) {
-  let x = 0;
+  let res = [];
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] != val) {
-      nums[x] = nums[i];
-      x++;
+      res.push(nums[i]);
     }
   }
 
-  return x;
+  return res.length;
 };
