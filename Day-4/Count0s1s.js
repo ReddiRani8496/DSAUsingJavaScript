73a8aa44bdb2c951a7b6f48ae02f3e2ed5cf486c
reddirani8496 <reddiranibattuluri@gmail.com>
2026-04-02 21:51:00 +0530

    Count 0s and 1s

---
+++ Day-4/Count0s1s.js
@@ -0,0 +1,11 @@
+let arr = [1, 0, 0, 1, 0, 1, 1];
+let zeroes = 0;
+let ones = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] == 0) zeroes++;
+  else ones++;
+}
+
+console.log(zeroes);
+console.log(ones);
