f781ccb37089a2ac187713cbb50909256b466bde
reddirani8496 <reddiranibattuluri@gmail.com>
2026-04-09 22:49:06 +0530

    Merger sorted array approach-2 uploaded

--- Day-4/MergeSortedArray.js
+++ Day-4/MergeSortedArray.js
@@ -24,6 +24,32 @@
 
 // brute force approach
 
+// /**
+//  * @param {number[]} nums1
+//  * @param {number} m
+//  * @param {number[]} nums2
+//  * @param {number} n
+//  * @return {void} Do not return anything, modify nums1 in-place instead.
+//  */
+// var merge = function (nums1, m, nums2, n) {
+//   let j = 0;
+//   for (let i = m; i < nums1.length; i++) {
+//     nums1[i] = nums2[j++];
+//   }
+
+//   for (let i = 0; i < nums1.length; i++) {
+//     for (let k = 0; k < nums1.length - i - 1; k++) {
+//       if (nums1[k] > nums1[k + 1]) {
+//         let temp = nums1[k];
+//         nums1[k] = nums1[k + 1];
+//         nums1[k + 1] = temp;
+//       }
+//     }
+//   }
+//   console.log(nums1);
+// };
+
+// Approach - 2 using extra array
 /**
  * @param {number[]} nums1
  * @param {number} m
@@ -33,18 +59,30 @@
  */
 var merge = function (nums1, m, nums2, n) {
   let j = 0;
-  for (let i = m; i < nums1.length; i++) {
-    nums1[i] = nums2[j++];
-  }
-
-  for (let i = 0; i < nums1.length; i++) {
-    for (let k = 0; k < nums1.length - i - 1; k++) {
-      if (nums1[k] > nums1[k + 1]) {
-        let temp = nums1[k];
-        nums1[k] = nums1[k + 1];
-        nums1[k + 1] = temp;
-      }
+  let extra = nums1.splice(0, m);
+  let i = 0;
+  let k = 0;
+  while (i < extra.length && j < n) {
+    if (extra[i] < nums2[j]) {
+      nums1[k] = extra[i];
+      i++;
+      k++;
+    } else {
+      nums1[k] = nums2[j];
+      k++;
+      j++;
     }
   }
+
+  while (i < extra.length) {
+    nums1[k++] = extra[i++];
+  }
+
+  while (j < n) {
+    nums1[k++] = nums2[j++];
+  }
+
   console.log(nums1);
 };
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
