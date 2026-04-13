93e9a829f01799c86be829f371275c4877298095
reddirani8496 <reddiranibattuluri@gmail.com>
2026-04-13 09:28:08 +0530

    InsertionSort.js

--- Day-6/InsertionSort.js
+++ Day-6/InsertionSort.js
@@ -1,16 +1,33 @@
+// function selectionSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let j = i + 1;
+//     while (j >= 0) {
+//       if (arr[j] < arr[i]) {
+//         let temp = arr[j];
+//         arr[j] = arr[i];
+//         arr[i] = temp;
+//       } else {
+//         break;
+//       }
+//       j--;
+//     }
+//   }
+
+//   return arr;
+// }
+
+// console.log(selectionSort([3, 1, 4, 2, 5]));
+
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    let j = i + 1;
-    while (j >= 0) {
-      if (arr[j] < arr[i]) {
-        let temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-      } else {
-        break;
-      }
-      j--;
+    let prev = i - 1;
+    let cur = arr[i];
+    while (prev >= 0 && arr[prev] > cur) {
+      arr[prev + 1] = arr[prev];
+      prev--;
     }
+
+    arr[prev + 1] = cur;
   }
 
   return arr;
