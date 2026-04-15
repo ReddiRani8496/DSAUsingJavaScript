0345d641ee9d3a1cc3ac63da96981888545863e1
reddirani8496 <reddiranibattuluri@gmail.com>
2026-04-15 21:58:34 +0530

    MergeSort.js

--- Day-6/MergeSort.js
+++ Day-6/MergeSort.js
@@ -1,23 +1,3 @@
-// function selectionSort(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     let j = i + 1;
-//     while (j >= 0) {
-//       if (arr[j] < arr[i]) {
-//         let temp = arr[j];
-//         arr[j] = arr[i];
-//         arr[i] = temp;
-//       } else {
-//         break;
-//       }
-//       j--;
-//     }
-//   }
-
-//   return arr;
-// }
-
-// console.log(selectionSort([3, 1, 4, 2, 5]));
-
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let prev = i - 1;
