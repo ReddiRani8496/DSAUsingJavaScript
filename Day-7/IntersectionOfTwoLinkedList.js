452aba6578485ba71542430469529939b25e10ec
reddirani8496 <reddiranibattuluri@gmail.com>
2026-04-24 00:16:17 +0530

    Linkedlist programs uploaded

---
+++ Day-7/IntersectionOfTwoLinkedList.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  let l1 = headA;
+  let l2 = headB;
+  while (l1 != l2) {
+    if (l1) {
+      l1 = l1.next;
+    } else {
+      l1 = headB;
+    }
+
+    if (l2) {
+      l2 = l2.next;
+    } else {
+      l2 = headA;
+    }
+  }
+  return l1;
+};
