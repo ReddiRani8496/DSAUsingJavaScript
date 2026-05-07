087658bee1d78c793789c01ce141b2553454b8f7
reddirani8496 <reddiranibattuluri@gmail.com>
2026-05-07 07:24:54 +0530

    string programs uploaded

--- Day-8/ReverseString/Appr2.js
+++ Day-8/ReverseString/Appr2.js
@@ -1,10 +1,14 @@
-var reverseStr = function (s, k) {
-  let arr = s.split("");
+var reverseStr = function(s, k) {
+    let arr = s.split("");
+    for(let i = 0;i<s.length;i = i+(2*k)) {
+        let n = k;
+        let mid = Math.floor(n/2);
+        for(let j = 0;j<mid;j++) {
+            let temp = arr[i+j];
+            arr[i+j] = arr[i+n-j-1];
+            arr[i+n-j-1] = temp;
+        }
 
-  for (let i = 0; i < arr.length; i += 2 * k) {
-    let reversedPart = arr.slice(i, i + k).reverse();
-    arr.splice(i, k, ...reversedPart);
-  }
-
-  return arr.join("");
+    }
+    return arr.join("");
 };
