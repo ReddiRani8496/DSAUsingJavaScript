5bad47c9d13632728b0967faf95e9fcd379760b1
reddirani8496 <reddiranibattuluri@gmail.com>
2026-05-09 22:32:55 +0530

    map methods pushed

---
+++ Map/methods.js
@@ -0,0 +1,25 @@
+let map = new Map();
+map.set("a", 1);
+map.set("b", 2);
+
+console.log(map.size);
+
+console.log(map.get("b"));
+
+console.log(map.has("a"));
+
+console.log(map.delete("a"));
+
+map.set("c", 3);
+
+for (let key of map.keys()) {
+  console.log(key);
+}
+
+for (let value of map.values()) {
+  console.log(value);
+}
+
+for (let [key, value] of map) {
+  console.log(key, " ", value);
+}
