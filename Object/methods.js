64e0778ed80c13e2674a8428d1ab406c9f52687f
reddirani8496 <reddiranibattuluri@gmail.com>
2026-05-12 22:36:31 +0530

    updated object methods

---
+++ Object/methods.js
@@ -0,0 +1,16 @@
+let person = {
+  name: "Rani",
+  age: 22,
+  city: "Hyderabad",
+};
+
+console.log(person);
+
+//Method 2: Using new Object()
+
+let car = new Object();
+
+car.brand = "Toyota";
+car.model = "Camry";
+
+console.log(car);
