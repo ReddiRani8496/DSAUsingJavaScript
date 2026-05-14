77e41c955aa3cfd696eafc2b59bb54d4b2b95623
reddirani8496 <reddiranibattuluri@gmail.com>
2026-05-14 23:04:33 +0530

    pushed object methods

--- Object/methods.js
+++ Object/methods.js
@@ -15,4 +15,24 @@ car.model = "Camry";
 
 console.log(car);
 
-console.log(car[brand]);
+console.log(car["brand"]);
+console.log(car["model"]);
+
+// add new values to object
+let obj = {};
+obj.name = "Rani";
+obj.age = 25;
+console.log(obj);
+
+// print keys
+for (let key in obj) {
+  console.log(key);
+}
+
+// print values
+for (let key in obj) {
+  console.log(obj[key]);
+}
+
+// print only values using Object.values
+console.log(Object.values(obj));
