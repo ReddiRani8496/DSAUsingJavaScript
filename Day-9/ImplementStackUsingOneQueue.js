6f45e9a6e329b131e875149a7f08abee64aed262
reddirani8496 <reddiranibattuluri@gmail.com>
2026-05-20 23:10:17 +0530

    pushed Implement stack using one queue

---
+++ Day-9/ImplementStackUsingOneQueue.js
@@ -0,0 +1,52 @@
+var MyStack = function () {
+  this.q1 = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+  this.q1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+  let size = this.q1.length;
+  for (let i = 0; i < size - 1; i++) {
+    this.q1.push(this.q1.shift());
+  }
+  let element = this.q1.shift();
+  return element;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  let size = this.q1.length;
+  for (let i = 0; i < size - 1; i++) {
+    this.q1.push(this.q1.shift());
+  }
+  let element = this.q1[0];
+  this.q1.push(this.q1.shift());
+  return element;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+  return this.q1.length == 0;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
