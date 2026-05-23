9ece433167965f16aac9ae85a35f7b453467be1d
reddirani8496 <reddiranibattuluri@gmail.com>
2026-05-23 22:13:59 +0530

    uploaded stack programs

---
+++ Day-9/MinStack.js
@@ -0,0 +1,46 @@
+var MinStack = function () {
+  this.stack = [];
+  this.minStack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
+
+  if (
+    this.minStack.length === 0 ||
+    val <= this.minStack[this.minStack.length - 1]
+  ) {
+    this.minStack.push(val);
+  }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  let removed = this.stack.pop();
+
+  if (removed === this.minStack[this.minStack.length - 1]) {
+    this.minStack.pop();
+  }
+
+  return removed;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1];
+};
---
+++ Day-9/ValidParenthishs.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
+      arr.push(s[i]);
+    } else if (s[i] == ")") {
+      if (arr[arr.length - 1] == "(") {
+        arr.pop();
+      } else {
+        return false;
+      }
+    } else if (s[i] == "}") {
+      if (arr[arr.length - 1] == "{") {
+        arr.pop();
+      } else {
+        return false;
+      }
+    } else if (s[i] == "]") {
+      if (arr[arr.length - 1] == "[") {
+        arr.pop();
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return arr.length == 0;
+};
