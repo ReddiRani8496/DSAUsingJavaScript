9ea322e1eb6e69d070ecbea0e45bc5a8f6caaa26
reddirani8496 <reddiranibattuluri@gmail.com>
2026-06-24 23:24:51 +0530

    Next greater element uploaded

--- Day-9/NextGreaterElement.js
+++ Day-9/NextGreaterElement.js
@@ -1,1 +1,17 @@
-var nextGreaterElement = function (nums1, nums2) {};
+var nextGreaterElement = function (nums1, nums2) {
+  const stack = [];
+  const nextGreaterMap = new Map();
+
+  for (let num of nums2) {
+    while (stack.length && num > stack[stack.length - 1]) {
+      nextGreaterMap.set(stack.pop(), num);
+    }
+    stack.push(num);
+  }
+
+  while (stack.length) {
+    nextGreaterMap.set(stack.pop(), -1);
+  }
+
+  return nums1.map((num) => nextGreaterMap.get(num));
+};
