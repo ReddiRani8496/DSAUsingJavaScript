b718aecb069487bebf227fa6a64ab498e5dd2029
reddirani8496 <reddiranibattuluri@gmail.com>
2026-07-07 22:19:55 +0530

    debouncing file uploaded

--- Object/debouncing.js
+++ Object/debouncing.js
@@ -8,12 +8,8 @@ function debounce(func, delay) {
   let timeoutId;
 
   return function (...args) {
-    // 1. Clear the existing timer if the function is called again before the delay finishes
     clearTimeout(timeoutId);
-
-    // 2. Set a new timer
     timeoutId = setTimeout(() => {
-      // 3. Execute the function with the correct context (this) and arguments
       func.apply(this, args);
     }, delay);
   };
